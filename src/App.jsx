12947a0a4908ab4c7a1063622533d8b574a805b0
Celine <chelinv2004@gmail.com>
2025-02-15 20:57:29 -0500

    front end1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = 'http://127.0.0.1:5000/alumnos';
+const API_URL = 'http://127.0.0.1:5000/api/alumnos';
 
 function App() {
   const [alumnos, setAlumnos] = useState([]);
